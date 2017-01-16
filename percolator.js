5cb962c97606ade402d982dd50ac2ad0f8568378
Dave Solomon <dave.c.solomon@gmail.com>
2017-01-16 09:54:29 -0500

    Bare-ish files.

---
+++ percolator.js
@@ -0,0 +1,23 @@
+var percolator = {
+    membershipId: undefined,
+    getBungieData: function() {
+
+    },
+    fetchMembershipIdFromBungie: function(userName, systemId) {
+
+    },
+    getMembershipId: function() {
+        if(this.membershipId) {
+            return this.membershipId;
+        }
+    },
+    setMembershipId: function(membershipId) {
+
+    },
+    getAccountSummary: function(membershipId) {
+
+    },
+    initEvents: function() {
+
+    }
+};
